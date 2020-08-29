8d3f89a9abfa4b3d7f195f73b9d27f571713419b
Chen Chai <chen.chai@outlook.com>
2020-08-29 18:04:00 -0400

    Changed status from cancelled to 'no sections found' if no classOfferings found

--- catalog/views.py
+++ catalog/views.py
@@ -221,7 +221,9 @@ def courseDetail(request, subject, code):
                     else: 
                         instructors = None
                     
-                    if allClassesCancelled == True:
+                    if len(enrollmentDict.get(offering.term, {})) == 0:
+                        status = 'no sections found'
+                    elif allClassesCancelled == True:
                         status = 'cancelled'
                     else:
                         status = 'offered'
